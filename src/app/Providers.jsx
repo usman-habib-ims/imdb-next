6c8f25da1e65ed5e58f3608703f52cdfea91baf3
Usman Habib <usmanhabib802@gmail.com>
2025-04-18 23:13:14 +0500

    Create the navbar component

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from "next-themes";
 export default function Providers({ children }) {
   return (
     <ThemeProvider defaultTheme="system" attribute="class">
-      <div className="text-gray-700 dark:text-gray-200 dark-gray-700 min-h-screen select-none transition-colors duration-300">
+      <div className="text-gray-600 dark:text-gray-100 dark-gray-700 min-h-screen select-none transition-colors duration-300">
         {children}
       </div>
     </ThemeProvider>
