734d330cc485af1f1b58e3baeaf5ec52ed530f39
Usman Habib <usmanhabib802@gmail.com>
2025-04-22 00:03:11 +0500

    fetch data from tmdb

---
+++ src/components/Results.jsx
@@ -0,0 +1,29 @@
+// import React from 'react'
+
+// export default function Results({results}) {
+//   return (
+//     <div>
+//         {
+//             results.map((result) => (
+//                 <div key={result.id}>
+//                     <h2>{result.original_title}</h2>
+//                 </div>
+//             ))
+//         }
+//     </div>
+//   )
+// }
+
+import React from "react";
+
+export default function Results({ results }) {
+  return (
+    <div>
+      {results.map((result) => (
+        <div key={result.id}>
+          <h2>{result.original_title || result.name}</h2>
+        </div>
+      ))}
+    </div>
+  );
+}
