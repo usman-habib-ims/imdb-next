1f4fe38ac7e4a9d5a1e21121609768f1077a891d
Usman Habib <usmanhabib802@gmail.com>
2025-04-28 12:48:30 +0500

    add search box and also major changes

--- src/app/page.js
+++ src/app/page.js
@@ -22,27 +22,27 @@
 //   )
 // }
 
-import Results from "@/components/Results";
-import { resolve } from "styled-jsx/css";
+import Results from '@/components/Results';
 
 const API_KEY = process.env.API_KEY;
 
 export default async function Home({ searchParams }) {
-  const genre = searchParams?.genre || "fetchTrending";
-
+  const genre = searchParams.genre || 'fetchTrending';
   const res = await fetch(
     `https://api.themoviedb.org/3${
-      genre === "fetchTopRated" ? "/movie/top_rated" : "/trending/all/week"
+      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
     }?api_key=${API_KEY}&language=en-US&page=1`,
     { next: { revalidate: 10000 } }
   );
-
   const data = await res.json();
-  const result = data.results;
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  const results = data.results;
 
   return (
     <div>
-      <Results results={result} />
+      <Results results={results} />
     </div>
   );
 }
